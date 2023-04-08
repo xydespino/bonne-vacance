414825c0ec91b76ae32547fd6b4882dbb4f1e845
xydespino <xydespino@outlook.com>
2023-04-08 11:33:54 -0300

    Implementing Google Maps API and Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Grid } from '@material-ui/core';
 
 
 import Header from './components/Header/Header';
@@ -11,9 +11,23 @@ import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing ={3} style={{width: '100'}}>
+            <Grid item xs={12} md={4}>
+                <List />
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Map />
+            </Grid>
+        </Grid>
+        
+        
+
+
+
+        </>
     )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
 
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 44.8857, lng: 63.1005};
+
     return(
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBeO73blmeUIUROnm3Oct63o43_R3XBG64'}}    
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={10}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+            </GoogleMapReact>
+
+        </div>
     )
 }
 
